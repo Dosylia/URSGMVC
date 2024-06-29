c5945e1c6f34756b9562a0e4641d0d7d8fc232bf
Dosylia <emma.montbarbon@outlook.fr>
2024-06-29 06:59:46 +0200

    Added accepting/rejecting friend requests

--- controllers/FriendRequestController.php
+++ controllers/FriendRequestController.php
@@ -14,6 +14,7 @@ class FriendRequestController
     private FriendRequest $friendrequest;
     private User $user;
     private ChatMessage $chatmessage;
+    private $frId;
 
     
     public function __construct()
@@ -21,6 +22,61 @@ class FriendRequestController
         $this -> friendrequest = new FriendRequest();
         $this -> user = new User();
         $this -> chatmessage = new ChatMessage();
+    }
+
+    public function acceptFriendRequest()
+    {
+        $frId = $this->validateInput($_GET["fr_id"]);
+        $this->setFrId($frId);
+
+        $updateStatus = $this-> friendrequest -> acceptFriendRequest($this->getFrId());
+
+        if ($updateStatus)
+        {
+            header("location:index.php?action=userProfile&message=Friend request accepted");
+            exit();  
+        }
+        else
+        {
+            header("location:index.php?action=userProfile&message=Could not accept it");
+            exit();
+        }
+    }
+
+    public function rejectFriendRequest()
+    {
+        $frId = $this->validateInput($_GET["fr_id"]);
+        $this->setFrId($frId);
+
+        $updateStatus = $this-> friendrequest -> rejectFriendRequest($this->getFrId());
+
+        if ($updateStatus)
+        {
+            header("location:index.php?action=userProfile&message=Friend request rejected");
+            exit();  
+        }
+        else
+        {
+            header("location:index.php?action=userProfile&message=Could not accept it");
+            exit();
+        }
+    }
+
+    public function validateInput($input) 
+    {
+        $input = trim($input);
+        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+        return $input;
+    }
+
+    public function getFrId()
+    {
+        return $this->frId;
+    }
+
+    public function setFrId($frId)
+    {
+        $this->frId = $frId;
     }
 
 }
