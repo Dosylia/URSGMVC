e88507d5650d2331ec1ee13ab8fec4ee2966e857
Dosylia <emma.montbarbon@outlook.fr>
2025-08-19 20:50:14 +0200

    Forgot refacto of one function

--- controllers/FriendRequestController.php
+++ controllers/FriendRequestController.php
@@ -221,15 +221,10 @@ class FriendRequestController
             $userId = $_POST['userId'];
             $this->setUserId((int)$userId);
 
-            // Validate Authorization Header
-            
-
-            if (!$authHeader || !preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
-                echo json_encode(['success' => false, 'error' => 'Unauthorized']);
+            $token = $this->getBearerTokenOrJsonError();
+            if (!$token) {
                 return;
             }
-
-            $token = $matches[1];
 
             // Validate Token for User
             if (!$this->validateTokenWebsite($token, $userId)) {
